71345351e32c2db6fbd479427b303e9c22d836e8
niotho <dtnbah@gmail.com>
2024-02-08 14:29:34 +0000

    Creating the project

---
+++ StatisticProject/StatisticProject/src/main/java/com/stats/StatisticProject/StatisticProjectApplication.java
@@ -0,0 +1,13 @@
+package com.stats.StatisticProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticProjectApplication.class, args);
+	}
+
+}
---
+++ StatisticProject/StatisticProject/src/test/java/com/stats/StatisticProject/StatisticProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stats.StatisticProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
